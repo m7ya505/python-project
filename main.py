47c13a5dfe976fd401b06680394216de40a9e637
Mohammed-Alqahtani <m7ya505@gmail.com>
2026-08-03 12:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+def calc_total(bill, tip_percent):
+    return bill + bill * tip_percent / 100
+
+
+def main():
+    bill = float(input("bill: "))
+    pct = float(input("tip %: "))
+    total = calc_total(bill, pct)
+    print(f"total = {total}")
+
+    # إضافة شرط تحكم في التدفق (if)
+    if total > 200:
+        print("generous tip!")
+
+
+if __name__ == "__main__":
+    main()
